e3fbb9868d0810e4cb4d00067780cacc0ac67f2c
WB Vera <WBVera@rosariobus.com.ar>
2023-05-18 15:09:02 -0300

    Se puede redirigir a otra pantalla

---
+++ FrontEnd/src/app/Usuario.ts
@@ -0,0 +1,6 @@
+export interface Usuario{
+    ok : boolean;
+    usuarioBloqueado : boolean,
+    mensaje : string,
+    numeroTarjeta : number
+}
--- FrontEnd/src/app/pantalla-principal/pantalla-principal.component.ts
+++ FrontEnd/src/app/pantalla-principal/pantalla-principal.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { RestService } from '../rest.service';
+import { map } from 'rxjs/operators';
+import { Usuario } from '../Usuario';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-pantalla-principal',
   templateUrl: './pantalla-principal.component.html',
@@ -9,7 +13,10 @@ import { RestService } from '../rest.service';
 export class PantallaPrincipalComponent implements OnInit {
   myValue: string;
 
-  constructor(private http : HttpClient,private restService : RestService) { 
+  constructor(
+    private http : HttpClient,
+    private restService : RestService,
+    private router: Router) { 
     this.myValue = '';
   } 
   ngOnInit(): void {
@@ -20,5 +27,18 @@ export class PantallaPrincipalComponent implements OnInit {
   public cargarData(){
     this.restService.get(`https://localhost:44363/api/Usuario/GetUsuarioByUsuarioTarjetaAsync?input=${this.myValue}`)
     .subscribe(respuesta => console.log(respuesta))
+  }
+  public convertirModelo(){
+    this.http.get(`https://localhost:44363/api/Usuario/GetUsuarioByUsuarioTarjetaAsync?input=${this.myValue}`).pipe(map(response => {
+        // Realiza la transformación del resultado en un objeto aquí
+        return response as Usuario;
+      })
+    ).subscribe(data => {
+      // El objeto transformado está disponible aquí
+      console.log(data);
+      if(data.ok == true){
+        this.router.navigateByUrl('/pantalla');
+      }
+    });
   }
 }
