063ea3fd07284e03aa034acdc90dbc552fe7c3b2
WB Vera <WBVera@rosariobus.com.ar>
2023-05-18 22:10:58 -0300

    Pre insert de component nuevo

--- FrontEnd/src/app/pantalla-principal/pantalla-principal.component.ts
+++ FrontEnd/src/app/pantalla-principal/pantalla-principal.component.ts
@@ -43,4 +43,5 @@ export class PantallaPrincipalComponent implements OnInit {
       }
     });
   }
+  
 }
--- FrontEnd/src/app/pantalla/pantalla.component.ts
+++ FrontEnd/src/app/pantalla/pantalla.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SharedDataServiceService } from '../shared-data-service.service';
 import { Injectable } from '@angular/core';
 import { Usuario } from '../Usuario';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-pantalla',
@@ -12,7 +13,7 @@ import { Usuario } from '../Usuario';
   providedIn: 'root'
 })
 export class PantallaComponent implements OnInit {
-  constructor(private sharedDataService: SharedDataServiceService) { 
+  constructor(private sharedDataService: SharedDataServiceService, private http : HttpClient) { 
     
     // const storedValorRespuesta = localStorage.getItem(this.clave);
     // console.log(storedValorRespuesta)
@@ -23,8 +24,9 @@ export class PantallaComponent implements OnInit {
   }
   clave : string = 'myFinalKey';
   myValue: string = "";
+  PIN : number = 0;
   valor : Usuario | null = this.sharedDataService.valorRespuesta
-  intValor : number | undefined = this.valor?.numeroTarjeta
+  ValorNumeroTarjeta : number | undefined = this.valor?.numeroTarjeta
 
   ngOnInit(): void {
     // localStorage.setItem(this.clave, JSON.stringify(this.valor))
@@ -33,11 +35,12 @@ export class PantallaComponent implements OnInit {
     if(storedValorRespuesta){
       this.valor = JSON.parse(storedValorRespuesta)
       console.log(`Este valor viene del storage ${this.valor}`);
-      this.intValor = this.valor?.numeroTarjeta
-      console.log(this.intValor);
+      this.ValorNumeroTarjeta = this.valor?.numeroTarjeta
+      console.log(this.ValorNumeroTarjeta);
     }
   }
-  sendData(value: string) {
-    // console.log('Valor ingresado:', value);
+  public GetAccessByUsuarioAsync(){
+    this.http.get(`https://localhost:44363/api/Usuario/GetAccessByUsuarioAsync?numeroTarjeta=${this.ValorNumeroTarjeta}&PIN=${this.PIN}`)
+    .subscribe(respuesta => console.log(respuesta))
   }
 }
